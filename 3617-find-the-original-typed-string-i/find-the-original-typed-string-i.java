8af24f7ededfb5db31010ba42f9793a527ca607c
udhyaudhya4002 <udhayaprof@gmail.com>
2025-07-01 11:36:54 +0530

    Time: 6 ms (2.48%) | Memory: 42.8 MB (7.44%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int possibleStringCount(String w) {
+        int[] fre=new int[26];
+        char ch[]=w.toCharArray();
+        for(int i=1;i<ch.length;i++){
+            if(ch[i]==ch[i-1]){
+            fre[ch[i]-'a']++;
+            }
+        }
+        int ans=1;
+        for(int val: fre){
+            if(val>=1){
+                ans+=val;
+            }
+        }
+       System.out.print(Arrays.toString(fre));
+        return ans;
+    }
+}
