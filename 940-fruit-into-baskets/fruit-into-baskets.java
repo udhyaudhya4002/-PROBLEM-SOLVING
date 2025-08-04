a3e92171577bb077a6f814871cc384baeda3c9e5
udhyaudhya4002 <udhayaprof@gmail.com>
2025-08-04 16:00:06 +0530

    Time: 49 ms (76.68%) | Memory: 55.3 MB (40.84%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int totalFruit(int[] f) {
+        int s = 0, e = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int n = f.length, max = 0;
+
+        while (e < n) {
+            // Add current fruit to the map
+            map.put(f[e], map.getOrDefault(f[e], 0) + 1);
+
+            // If we have more than 2 fruit types, shrink window
+            while (map.size() >= 3) {
+                map.put(f[s], map.get(f[s]) - 1);
+                if (map.get(f[s]) == 0) {
+                    map.remove(f[s]);
+                }
+                s++;
+            }
+
+            // Calculate window size
+            int cmax = e - s + 1;
+            max = Math.max(cmax, max);
+            e++;
+        }
+
+        return max;
+    }
+}
